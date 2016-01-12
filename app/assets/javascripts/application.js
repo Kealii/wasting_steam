05bbfc447f388446480fa05f593cbea92bffba3c
Michael Wong <michael.brian.wong@gmail.com>
2016-01-12 10:59:08 -0700

    Replaces CDNs with Bootstrap Sass Gem

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,5 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
+//= require bootstrap-sprockets
 //= require scripts
 //= require_tree .
