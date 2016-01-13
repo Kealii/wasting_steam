5fc8fc6b39017ce11387467761fc972c3f16f571
Michael Wong <michael.brian.wong@gmail.com>
2016-01-13 13:47:59 -0700

    Retrieve Steam ID's for all games played for less than 5 minutes'

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -4,6 +4,13 @@ class ResultsController < ApplicationController
       url = URI.parse("http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=#{ENV['STEAM_API_KEY']}&steamid=#{current_user.uid}&format=json")
       response = Net::HTTP::get(url)
       @results = JSON.load(response)['response']['games']
+      @unplayed_ids = []
+      @results.each do |results|
+        if results['playtime_forever'] <= 5
+          @unplayed_ids << results['appid']
+        end
+      end
+      @total = @unplayed_ids.count
     end
   end
 end
