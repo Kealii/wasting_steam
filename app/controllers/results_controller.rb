8816ad647c437f36353ad4e11a9df2d1d13bfde5
Michael Wong <michael.brian.wong@gmail.com>
2016-01-13 13:38:08 -0700

    added methods to response controller to return a json list of data for a user

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,6 +1,9 @@
 class ResultsController < ApplicationController
   def index
-    @results = ['nada']
-    @total = @results.count
+    if current_user
+      url = URI.parse("http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=#{ENV['STEAM_API_KEY']}&steamid=#{current_user.uid}&format=json")
+      response = Net::HTTP::get(url)
+      @results = JSON.load(response)['response']['games']
+    end
   end
 end
