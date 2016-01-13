4f41289e913bc2ecf6fea0f9ee9a6b2220c7cd1c
Michael Wong <michael.brian.wong@gmail.com>
2016-01-13 16:24:48 -0700

    Returns the total dollar value of all unplayed games in your account and displays total, number of unplayed games, and list of unplayed games

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -3,6 +3,7 @@ class ResultsController < ApplicationController
     if current_user
       @unplayed_ids = []
       @names        = []
+      @wasted_money = 0
       url = URI.parse("http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=#{ENV['STEAM_API_KEY']}&steamid=#{current_user.uid}&format=json")
       response = Net::HTTP::get(url)
       @results = JSON.load(response)['response']['games']
@@ -17,6 +18,9 @@ class ResultsController < ApplicationController
         @game = JSON.load(response)["#{id}"]
         if @game['data']
           @names << @game['data']['name']
+          if @game['data']['price_overview']
+            @wasted_money += @game['data']['price_overview']['initial']
+          end
         end
       end
     end
