9401194258fc5cd188f2e990f490498d67f2488c
Michael Wong <michael.brian.wong@gmail.com>
2016-01-14 13:53:52 -0700

    Adds Logout test and additional parameters for login test

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -4,13 +4,26 @@ feature 'testing oauth' do
 
   before :each do
     OmniAuth.config.mock_auth[:steam] = nil
+    stub_omniauth
   end
 
   scenario 'login with steam' do
-    stub_omniauth
     visit login_path
 
     expect(current_path).to eq(waste_analysis_path)
+    expect(page).to have_content('Hello Testing Steam')
+    expect(page).to have_content('Click Here To Analyze')
+    expect(page).to have_content('Logout')
+  end
+
+  scenario 'logout with steam' do
+    visit login_path
+    visit logout_path
+
+    expect(current_path).to eq(root_path)
+    expect(page).to_not have_content('Hello Testing Steam')
+    expect(page).to_not have_content('Logout')
+    expect(page).to     have_content('Login')
   end
 
 end
