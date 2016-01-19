2b1d44f3e694a18c5181d0737ff7e580aa49a122
Michael Wong <michael.brian.wong@gmail.com>
2016-01-19 10:15:50 -0700

    Adds basic table styling

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,29 +1,39 @@
 class ResultsController < ApplicationController
   def index
     if current_user
-      @unplayed_ids = []
       @names        = []
       @wasted_money = 0
-      url = URI.parse("http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=#{ENV['STEAM_API_KEY']}&steamid=#{current_user.uid}&format=json")
-      response = Net::HTTP::get(url)
-      @results = JSON.load(response)['response']['games']
-      @results.each do |results|
-        if results['playtime_forever'] < 5
-          @unplayed_ids << results['appid']
-        end
+      find_games
+      find_prices
+    end
+  end
+
+
+  def find_games
+    @unplayed_ids = []
+    url = URI.parse("http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=#{ENV['STEAM_API_KEY']}&steamid=#{current_user.uid}&format=json")
+    response = Net::HTTP::get(url)
+    games = JSON.load(response)['response']['games']
+    games.each do |game|
+      if game['playtime_forever'] < 5
+        @unplayed_ids << game['appid']
       end
-      @unplayed_ids.each do |id|
-        url      = URI.parse("https://store.steampowered.com/api/appdetails/?appids=#{id}")
-        response = Net::HTTP::get(url)
-        @game = JSON.load(response)["#{id}"]
-        if @game['data']
-          @names << @game['data']['name']
-          if @game['data']['price_overview']
-            @wasted_money += @game['data']['price_overview']['initial']
-          end
+    end
+  end
+
+  def find_prices
+    @unplayed_ids.each do |id|
+      url      = URI.parse("https://store.steampowered.com/api/appdetails/?appids=#{id}")
+      response = Net::HTTP::get(url)
+      @game = JSON.load(response)["#{id}"]
+      if @game['data']
+        @names << @game['data']['name']
+        if @game['data']['price_overview']
+          @wasted_money += @game['data']['price_overview']['initial']
         end
       end
     end
   end
+
 end
 
