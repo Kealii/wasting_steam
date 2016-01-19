dcf20d3e3f6c9d589ded2965b3a8069532dc5827
Michael Wong <michael.brian.wong@gmail.com>
2016-01-19 12:16:13 -0700

    Formats data as a table and adds several field placeholders

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,8 +1,6 @@
 class ResultsController < ApplicationController
   def index
     if current_user
-      @names        = []
-      @wasted_money = 0
       find_games
       find_prices
     end
@@ -11,9 +9,9 @@ class ResultsController < ApplicationController
 
   def find_games
     @unplayed_ids = []
-    url = URI.parse("http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=#{ENV['STEAM_API_KEY']}&steamid=#{current_user.uid}&format=json")
+    url      = URI.parse("http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=#{ENV['STEAM_API_KEY']}&steamid=#{current_user.uid}&format=json")
     response = Net::HTTP::get(url)
-    games = JSON.load(response)['response']['games']
+    games    = JSON.load(response)['response']['games']
     games.each do |game|
       if game['playtime_forever'] < 5
         @unplayed_ids << game['appid']
@@ -22,15 +20,21 @@ class ResultsController < ApplicationController
   end
 
   def find_prices
+    @names        = []
+    @images       = []
+    @wasted_money = 0
     @unplayed_ids.each do |id|
       url      = URI.parse("https://store.steampowered.com/api/appdetails/?appids=#{id}")
       response = Net::HTTP::get(url)
-      @game = JSON.load(response)["#{id}"]
-      if @game['data']
-        @names << @game['data']['name']
-        if @game['data']['price_overview']
-          @wasted_money += @game['data']['price_overview']['initial']
-        end
+      game = JSON.load(response)["#{id}"]
+      if game['data']['name']
+        @names << game['data']['name']
+      end
+      if game['data']['price_overview']
+        @wasted_money += game['data']['price_overview']['initial']
+      end
+      if game['data']['header_image']
+        @images << game['data']['header_image']
       end
     end
   end
